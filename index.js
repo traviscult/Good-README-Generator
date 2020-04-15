f272f5351a04ab8f65b5ea61594ec86f5ea3836b
traviscult <traviscult@gmail.com>
2020-04-14 20:55:28 -0400

    generated questions and linked markdownfile

--- index.js
+++ index.js
@@ -2,28 +2,40 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require('util');
 const writeFileAsync = util.promisify(fs.writeFile);
+const generateMarkdown = require("./utils/generateMarkdown")
 
 const questions = () => {
     return inquirer.prompt([{
             type: "input",
             name: "gitHub",
-            message: "What is your GitHub Url?",
+            message: "What is your GitHub username?",
         },
         {
             type: "input",
             name: "title",
             message: "What is the title of your project?",
         },
-        // {
-        //     type: "input",
-        //     name: "",
-        //     message: "",
-        // },
-        // {
-        //     type: "input",
-        //     name: "",
-        //     message: "",
-        // },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of licnese would you like to have?",
+            choices: ['MIT', 'Microsoft Public License', "Mozilla Public License 2.0"]
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Explain what your app/project does?",
+        },
+        {
+            type: "input",
+            name: "",
+            message: "Explain what your app/project does?",
+        },
+        {
+            type: "input",
+            name: "",
+            message: "Explain what your app/project does?",
+        },
     ]);
 };
 
@@ -31,8 +43,10 @@ const questions = () => {
 function writeToFile(fileName, data) {}
 
 function init() {
-
+    questions();
+    // generateMarkdown();
+    // writeToFile();
 }
 
 init();
-questions();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,34 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+Description
+
+Table of contents UL LI
+
+Installation
+
+Usage 
+
+Credits
+
+License 
+
+Badge 
+
+Contributing
+
+
+Test
+
+
 `;
 }
+
+
+const projectURL = (gitHub, title) => {
+  const kebabCaseTitle = title.toLowerCase().split(" ").join("-");
+  return ` https://github.com/${gitHub}/${kebabCaseTitle}`
+
+};
 
 module.exports = generateMarkdown;
